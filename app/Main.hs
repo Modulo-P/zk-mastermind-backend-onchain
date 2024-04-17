65ae7e893e02e1fe6d69a7856f30c2834b8601f7
Juan Salvador Magán Valero <jmaganvalero@gmail.com>
2024-04-17 17:48:05 +0200

    feat: validate args

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ module Main where
 
 import Cardano.Api (PlutusScriptV2, writeFileTextEnvelope)
 import Cardano.Api.Shelley (PlutusScript (..))
+import Control.Monad (when)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Base16 as Base16
 import qualified Data.ByteString.Short as B
@@ -18,6 +19,7 @@ import Util (compiledScript)
 main :: IO ()
 main = do
   args <- getArgs
+  when (length args /= 1) $ error "Usage: mint-script <pubKeyHash>"
   let pubKeyHash = V2.PubKeyHash $ V2.toBuiltin $ fromJust $ decodeHex $ pack $ head args
   let serialisedScript = V2.serialiseCompiledCode $ compiledScript pubKeyHash
   B.writeFile "validator.uplc" . Base16.encode $ B.fromShort serialisedScript
