a7ca694d856ebc71c59f588eae96714decf3226b
Juan Salvador Magán Valero <jmaganvalero@gmail.com>
2024-04-18 16:42:59 +0200

    refactor: PubkeyHash generation

--- app/Main.hs
+++ app/Main.hs
@@ -8,21 +8,20 @@ import Cardano.Api.Shelley (PlutusScript (..))
 import Control.Monad (when)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Base16 as Base16
-import qualified Data.ByteString.Short as B
-import Data.Maybe (fromJust)
-import Data.Text (pack)
+import qualified Data.ByteString.Short as Bs
+import Data.String (IsString (fromString))
 import qualified PlutusLedgerApi.V2 as V2
 import System.Environment (getArgs)
-import Text.Hex (decodeHex)
 import Util (compiledScript)
 
 main :: IO ()
 main = do
   args <- getArgs
   when (length args /= 1) $ error "Usage: mint-script <pubKeyHash>"
-  let pubKeyHash = V2.PubKeyHash $ V2.toBuiltin $ fromJust $ decodeHex $ pack $ head args
+  let pubKeyHash = fromString $ head args
   let serialisedScript = V2.serialiseCompiledCode $ compiledScript pubKeyHash
-  B.writeFile "validator.uplc" . Base16.encode $ B.fromShort serialisedScript
+  putStrLn $ "PubKeyHash: " ++ show pubKeyHash
+  B.writeFile "validator.uplc" . Base16.encode $ Bs.fromShort serialisedScript
   result <- writeFileTextEnvelope "validator.plutus" Nothing (PlutusScriptSerialised serialisedScript :: PlutusScript PlutusScriptV2)
   case result of
     Left err -> putStrLn $ "Error: " ++ show err
